d5851b1e578e5310ec46b53c7b0ad0d718f84791
Ahmed Alsoufi <ahmedalsoufi44@gmail.com>
2026-06-02 03:14:04 +0300

    Remove stale Reanimated Babel plugin

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,9 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    // react-native-reanimated/plugin must be listed LAST. Required by
-    // react-native-reanimated (which @react-navigation/drawer depends on).
-    plugins: ["react-native-reanimated/plugin"]
+    presets: ["babel-preset-expo"]
   };
 };
